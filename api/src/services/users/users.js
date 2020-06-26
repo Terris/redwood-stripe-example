71a120ca8e4b77e6be6f91412de23d14e258c770
Terris Kremer <terris.kremer@gmail.com>
2020-06-25 23:00:39 -0400

    add stripe customers

--- api/src/services/users/users.js
+++ api/src/services/users/users.js
@@ -1,22 +1,27 @@
 import { db } from 'src/lib/db'
+import { requirePermission } from 'src/lib/auth'
 
 export const users = () => {
+  requirePermission('admin')
   return db.user.findMany()
 }
 
 export const user = ({ id }) => {
+  requirePermission('admin')
   return db.user.findOne({
     where: { id },
   })
 }
 
 export const createUser = ({ input }) => {
+  requirePermission('admin')
   return db.user.create({
     data: input,
   })
 }
 
 export const updateUser = ({ id, input }) => {
+  requirePermission('admin')
   return db.user.update({
     data: input,
     where: { id },
@@ -24,6 +29,7 @@ export const updateUser = ({ id, input }) => {
 }
 
 export const deleteUser = ({ id }) => {
+  requirePermission('admin')
   return db.user.delete({
     where: { id },
   })
