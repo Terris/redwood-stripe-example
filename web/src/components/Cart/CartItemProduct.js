2d2862401f15e2c147bb6fb3fdb89b14066ed70f
Terris Kremer <terris.kremer@gmail.com>
2020-07-01 14:55:48 -0400

    fixup! add cart

--- web/src/components/Cart/CartItemProduct.js
+++ web/src/components/Cart/CartItemProduct.js
@@ -3,6 +3,7 @@ export const CartItemProduct = ({ product }) => {
     <div className="cart-item-product">
       <div className="cart-item-product-name">{product.name}</div>
       <div className="cart-item-product-description">{product.description}</div>
+      <div className="cart-item-product-price">{product.unitAmount}</div>
     </div>
   )
 }
