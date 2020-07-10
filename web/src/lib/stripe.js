5edc74b730aa02d41d55991961846314393e982c
Terris Kremer <terris.kremer@gmail.com>
2020-07-10 08:24:01 -0400

    move public key to code

--- web/src/lib/stripe.js
+++ web/src/lib/stripe.js
@@ -1,6 +1,8 @@
 import { loadStripe } from '@stripe/stripe-js'
 
-export const stripePromise = loadStripe(process.env.REACT_APP_STRIPE_PUBLIC_KEY)
+export const stripePromise = loadStripe(
+  'pk_test_51GxylsEHtYUchzGVN9ErcrcFkPlFLgQ4bhXoSAcwW6itR5rTJWeXESUy90aGD4cbMYkZuwwVWEfVYmCytiZyqny200Sp0xu9hW'
+)
 
 export const CARD_ELEMENT_OPTIONS = {
   classes: {
