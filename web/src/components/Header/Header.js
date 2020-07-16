7f321db415099e9b7872f7f2eaf3d6ba6aa8a551
Terris Kremer <terris.kremer@gmail.com>
2020-07-16 12:36:05 -0400

    rm admin users

--- web/src/components/Header/Header.js
+++ web/src/components/Header/Header.js
@@ -14,7 +14,6 @@ const PrimaryNav = () => (
 
 const AdminNav = () => (
   <nav className="nav-admin">
-    <Link to={routes.adminUsers()}>Users</Link>
     <Link to={routes.adminProducts()}>Products</Link>
     <Link to={routes.adminOrders()}>Orders</Link>
   </nav>
